146f4373ae9ee66066b3fcc0d733c77b63b2c611
전세진 <sejin@jeonsejin-ui-MacBookPro.local>
2021-11-14 21:40:02 +0900

    hashCode overriding

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,19 @@
-import maputils.LazyMapExample;
+
+import hashcode.HashCodeOverrideExample;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        (new LazyMapExample()).showLazyMapExample();
+        try {
+//        (new LazyMapExample()).showLazyMapExample();
+            (new HashCodeOverrideExample()).showHashCodeOverrideExample();
+        } catch (final Exception e) {
+            System.err.println(e);
+            Arrays.stream(e.getStackTrace())
+                    .forEach(element -> System.err.println("    at " + element));
+        }
     }
 }
